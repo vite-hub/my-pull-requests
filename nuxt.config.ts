d9f9b975b82ed64c2383def313de60264c99bf2d
Max <maximogarciamtnez@gmail.com>
2026-08-21 16:00:30 +0000

    fix: expose Cloudflare preset to Nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2026-08-14',
 
   nitro: {
+    preset: process.env.VERCEL ? 'vercel' : 'cloudflare-module',
     wasm: {
       lazy: true,
     },
